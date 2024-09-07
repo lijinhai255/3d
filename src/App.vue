08330f0100de7e91c7a738e31cc7170ba11f1666
lijinhai255 <18854109500@163.com>
2024-09-07 11:06:11 +0800

    add:table

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <plan-list :data="userData.areaSales" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+import planList from "./components/TableList.vue";
+
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: "planList",
+  components: { planList },
+  props: {
+    data: Array,
+  },
+  setup() {
+    const userData = ref({
+      today: 0,
+      averageAge: 0,
+    });
+    return {
+      userData,
+    };
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-* {
-  padding: 0;
-  margin: 0;
+<style lang="scss" scoped>
+.plan-list {
+  width: 100%;
+  height: 100%;
+  background: rgb(55, 55, 55);
+  padding: 20px 40px;
+  box-sizing: border-box;
+
+  .title {
+    font-size: 36px;
+    margin-left: 20px;
+  }
+
+  .list {
+    width: 100%;
+    height: 880px;
+    margin-top: 20px;
+    padding: 30px 0;
+    box-sizing: border-box;
+    background: rgb(40, 40, 40);
+  }
 }
 </style>
